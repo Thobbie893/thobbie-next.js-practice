4de936366a51f9d668e8824fe5fb8218ba45d751
Thobbie 893 <egunjobithobbie4@gmail.com>
2026-03-17 23:14:04 +0100

    Updated

---
+++ app/components/Thobbie.jsx
@@ -0,0 +1,34 @@
+
+
+const Thobbie = (props) => {
+  return (
+    <section>
+    <div>
+      <h1>HTML</h1>
+      <p>sdp'fdsfkfkd;lfkd;lfkds;flkdsl;lf;a</p>
+    </div>
+
+    <div>
+      <h1>HTML</h1>
+      <p>sdp'fdsfkfkd;lfkd;lfkds;flkdsl;lf;a</p>
+    </div>
+
+    <div>
+      <h1>HTML</h1>
+      <p>sdp'fdsfkfkd;lfkd;lfkds;flkdsl;lf;a</p>
+    </div>
+
+    <div>
+      <h1>HTML</h1>
+      <p>sdp'fdsfkfkd;lfkd;lfkds;flkdsl;lf;a</p>
+    </div>
+    </section>
+  )
+}
+
+export default Thobbie
+
+
+
+
+
